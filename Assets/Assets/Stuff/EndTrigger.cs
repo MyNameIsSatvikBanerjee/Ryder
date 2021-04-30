1b54b3fba622829df27c75363b1a4fc996894a63
MyNameIsSatvikBanerjee <banerjeesatvik@gmail.com>
2021-04-30 16:59:36 +0530

    Added Main Menu , Pause Menu , Settings Menu
    Also added some more scened

---
+++ Assets/Assets/Stuff/EndTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+
+    public GameObject completeLevelUI;
+
+    void OnTriggerEnter()
+    {
+        completeLevelUI.SetActive(true);
+    }
+}
